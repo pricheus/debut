2756297cf5737386a5a63bbd6fab04d2c5c04ff6
Elie <precieuxbatonda@gmail.com>
2023-07-13 22:21:04 +0200

    add css with react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import testing from "./testing";
-function Comp() {
-  return <h1>Hey pricheus's there</h1>;
+
+//CSS
+import "./index.css";
+function Booklist() {
+  return (
+    <div className="bookList">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </div>
+  );
 }
 
-ReactDOM.render(<Comp />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article className="book">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/71h5+dpAriL._AC_UL600_SR600,400_.jpg"
+        alt="image of a bestseller"
+      />
+      <h2>Twisted Games</h2>
+      <p style={{ color: "inherit" }}>Ana Huang</p>
+    </article>
+  );
+};
 
-testing();
+ReactDOM.render(<Booklist />, document.getElementById("root"));
